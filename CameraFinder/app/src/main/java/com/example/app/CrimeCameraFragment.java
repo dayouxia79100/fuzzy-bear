c90249582ce5ffcb3d6cd81d61feaa576793f385
ming <han.568@osu.edu>
2014-01-02 16:42:00 -0500

    almost complete

---
+++ CameraFinder/app/src/main/java/com/example/app/CrimeCameraFragment.java
@@ -0,0 +1,121 @@
+package com.example.app;
+
+import android.annotation.TargetApi;
+import android.hardware.Camera;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by dayouxia on 1/2/14.
+ */
+public class CrimeCameraFragment extends Fragment {
+    private static final String TAG = "CrimeCameraFragment";
+
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+
+    private Camera.Size getBestSupportedSize(List<Camera.Size> sizes, int width, int height){
+        Camera.Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for(Camera.Size s: sizes){
+            int area = s.width * s.height;
+            if(area > largestArea){
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+        return bestSize;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime_camera,container,false);
+
+        Button takePictureButton = (Button) v.findViewById(R.id.crime_camera_takePictureButton);
+
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+
+        mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try{
+                    if(mCamera != null){
+                        mCamera.setPreviewDisplay(surfaceHolder);
+                    }
+                } catch (IOException e){
+                    Log.e("TAG", "Error setting up preview display",e);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+                if(mCamera == null) return;
+
+                Camera.Parameters parameters = mCamera.getParameters();
+                Camera.Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(),i2,i3);
+                parameters.setPreviewSize(s.width,s.height);
+                mCamera.setParameters(parameters);
+
+                try{
+                    mCamera.startPreview();;
+                } catch (Exception e){
+                    Log.e(TAG,"Could not start preview",e);
+                    mCamera.release();
+                    mCamera = null;
+
+                }
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                if(mCamera != null){
+                    mCamera.stopPreview();
+                }
+            }
+        });
+        return v;
+    }
+
+    @TargetApi(9)
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD){
+            mCamera = Camera.open(0);
+        } else{
+            mCamera = Camera.open();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+}
