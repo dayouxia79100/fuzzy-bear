171b914601e2dcb6478ea492ac0427f598b3b0db
ming <han.568@osu.edu>
2014-01-02 16:45:31 -0500

    Merge branch 'camera' into ActionBar

--- MyApplication2/app/src/main/java/com/example/app/Crime.java
+++ MyApplication2/app/src/main/java/com/example/app/Crime.java
@@ -1,5 +1,8 @@
 package com.example.app;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -11,6 +14,30 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+
+    private static final String JSON_ID = "id";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_SOLVED = "solved";
+    private static final String JSON_DATE = "date";
+
+    public JSONObject toJSON() throws JSONException{
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID,mId.toString());
+        json.put(JSON_TITLE,mTitle);
+        json.put(JSON_SOLVED,mSolved);
+        json.put(JSON_DATE, mDate.getTime());
+        return json;
+    }
+
+    public Crime(JSONObject json) throws JSONException{
+        mId = UUID.fromString(json.getString(JSON_ID));
+        if(json.has(JSON_TITLE)){
+            mTitle = json.getString(JSON_TITLE);
+        }
+        mSolved = json.getBoolean(JSON_SOLVED);
+        mDate = new Date(json.getLong(JSON_DATE));
+
+    }
 
     public Crime(){
         mId = UUID.randomUUID();
